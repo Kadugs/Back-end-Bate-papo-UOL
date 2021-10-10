f036a03ab51e94e52bac8fe5e7cebec63cf198ec
Kadugs <cadu20.gomes@gmail.com>
2021-10-10 12:14:12 -0300

    armazena o nome do usuario

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const users = []
+
+app.post('/participants', (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.send("OK");
+});
+
 app.listen(4000);
